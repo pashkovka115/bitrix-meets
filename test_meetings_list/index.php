ec2b72f82c74540a0711a4fe1068f77af7945594
m.kalenishin <m.kalenishin@ylab.io>
2022-07-14 13:01:22 +0300

    Компонент без фильра

--- test_meetings_list/index.php
+++ test_meetings_list/index.php
@@ -9,10 +9,17 @@ $APPLICATION->IncludeComponent(
   [
     'LIST_ID' => 'rooms_list',
     'ORM_NAME' => 'RoomTable',
-    'COLUMN_FIELDS' => [
-      'ID', 'NAME', 'ACTIVITY',
-      'INTEGRATION_ALIAS' => 'INTEGRATION.NAME',
-    ]
+    'COLUMN_FIELDS' => array(
+      0 => 'ID',
+      1 => 'NAME',
+      2 => 'ACTIVITY',
+      3 => 'INTEGRATION_ID',
+    ),
+    'FILTER_FIELDS' => array(
+      0 => 'ID',
+      1 => 'NAME',
+      2 => 'ACTIVITY',
+    )
   ]
 );
 ?>
@@ -24,14 +31,22 @@ $APPLICATION->IncludeComponent(
   [
     'LIST_ID' => 'integrations_list',
     'ORM_NAME' => 'IntegrationTable',
-    'COLUMN_FIELDS' => [
-      'ID', 'NAME', 'ACTIVITY',
-      'INTEGRATION_REF',
-      'LOGIN', 'PASSWORD',
-    ]
+    'COLUMN_FIELDS' => array(
+      0 => 'ID',
+      1 => 'NAME',
+      2 => 'ACTIVITY',
+      3 => 'INTEGRATION_REF',
+      4 => 'LOGIN',
+      5 => 'PASSWORD',
+    ),
+    'FILTER_FIELDS' => array(
+      0 => 'ID',
+      1 => 'NAME',
+      2 => 'ACTIVITY',
+    )
   ]
 );
 ?>
 
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
 
