b8a831d42e928109fdec1c0c855c58e69e852ba6
AKovalevskaya <a.kovalevskaya@ylab.io>
2022-07-14 15:46:23 +0300

    Написала php доки. Подключила стили bootstrap из ядра битрикс

--- local/modules/ylab.meetings/install/components/ylab/show.meeting/templates/.default/template.php
+++ local/modules/ylab.meetings/install/components/ylab/show.meeting/templates/.default/template.php
@@ -1,12 +1,9 @@
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
 <?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true){
     die();
 }
 use Bitrix\Main\Localization\Loc;
 
+\Bitrix\Main\UI\Extension::load("ui.bootstrap4");
 ?>
 <select id="select" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
     <?foreach($arResult as $arItems):?>  <!-- генерация доступных переговорных -->
