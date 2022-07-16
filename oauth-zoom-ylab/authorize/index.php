554765e380dfe91855f1ab82d0ddb4d224b31e27
Sergey <s.smirnov@ylab.io>
2022-07-16 08:50:57 +0300

    Добавил URL для авторизации с Zoom

---
+++ oauth-zoom-ylab/authorize/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+?>
+<?php $APPLICATION->IncludeComponent('ylab:meeting.oauth_redirect', '', []) ?>
