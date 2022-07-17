f26b6314a64b12f8e17de7f6515020294849f7d6
Sergey <s.smirnov@ylab.io>
2022-07-17 16:01:56 +0300

    Добавил примеры использования Zoom API

---
+++ oauth-zoom-ylab/examples/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+
+Bitrix\Main\Loader::includeModule('ylab.meetings');
+
+?>
+<p>
+    <a href="/oauth-zoom-ylab/examples/index.php?list=Y">List</a><br><br>
+</p>
+
+<?php
+if (isset($_GET['list'])){
+
+    $meeting = new \Ylab\Meeting\Zoom\Meeting();
+
+    $meetings = $meeting->list();
+    echo '<pre>';
+    print_r($meetings);
+    echo '</pre>';
+
+    foreach ($meetings->meetings as $item){
+        echo '<pre>===========';
+
+        print_r($meeting->getById($item->id));
+
+        echo '===========</pre>';
+    }
+}
---
+++ oauth-zoom-ylab/start/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+
+Bitrix\Main\Loader::includeModule('ylab.meetings');
+
+$settings = new \Ylab\Meeting\Zoom\Settings();
+
+$url = "https://zoom.us/oauth/authorize?response_type=code&client_id=".$settings->getClientId()."&redirect_uri=".$settings->getRedirectURI();
+?>
+
+<a href="<?= $url; ?>">Войти с помощью Zoom</a>
+
