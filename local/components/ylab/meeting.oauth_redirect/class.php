d7b452d9daa658f4ae60e4801f031371c63bc253
Sergey <s.smirnov@ylab.io>
2022-07-19 14:31:34 +0300

    Исправил ошибку namespace

--- local/components/ylab/meeting.oauth_redirect/class.php
+++ local/components/ylab/meeting.oauth_redirect/class.php
@@ -6,7 +6,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
-use Ylab\Meeting\Zoom\Auth;
+use Ylab\Meetings\Zoom\Auth;
 
 
 class YlabMeetingOauthRedirect extends CBitrixComponent
