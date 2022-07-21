ecb812fc8864a5c005ecf5c7113de28e7c9c61d4
m.kalenishin <m.kalenishin@ylab.io>
2022-07-21 11:27:24 +0300

    добавил в админку отображение комнат и интеграций

--- local/modules/ylab.meetings/admin/integrations.php
+++ local/modules/ylab.meetings/admin/integrations.php
@@ -2,4 +2,26 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin.php';
 ?>
 
-Empty list
+<?php
+$APPLICATION->IncludeComponent(
+  'ylab:meetings.list',
+  'grid',
+  [
+    'LIST_ID' => 'integrations_list',
+    'ORM_NAME' => 'IntegrationTable',
+    'COLUMN_FIELDS' => array(
+      0 => 'ID',
+      1 => 'NAME',
+      2 => 'ACTIVITY',
+      3 => 'INTEGRATION_REF',
+      4 => 'LOGIN',
+      5 => 'PASSWORD',
+    ),
+    'FILTER_FIELDS' => array(
+      0 => 'ID',
+      1 => 'NAME',
+      2 => 'ACTIVITY',
+    )
+  ],
+);
+?>
