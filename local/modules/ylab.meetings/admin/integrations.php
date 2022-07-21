d192e857eccfce8e9cb5eaa42cc4130784011fde
Maksim Zyryanov <m.zyryanov@ylab.io>
2022-07-21 16:30:14 +0700

    #5# вызов компонента грида в админке

--- local/modules/ylab.meetings/admin/integrations.php
+++ local/modules/ylab.meetings/admin/integrations.php
@@ -1,11 +1,12 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin.php';
 
+global $APPLICATION;
 $APPLICATION->IncludeComponent(
-	'ylab:integration.edit',
+	'ylab:meetings.integrations.list',
 	'grid',
 	[
-		'LIST_ID' => 'integrations.list',
+		'LIST_ID' => 'integrations_list',
 		'ORM_NAME' => 'IntegrationTable',
 		'COLUMN_FIELDS' => array(
 			0 => 'ID',
@@ -19,6 +20,9 @@ $APPLICATION->IncludeComponent(
 			0 => 'ID',
 			1 => 'NAME',
 			2 => 'ACTIVITY',
+			3 => 'INTEGRATION_REF',
+			4 => 'LOGIN',
+			5 => 'PASSWORD',
 		)
 	]
 );
