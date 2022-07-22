d1a2948429470e086bac762f42d988e8656ee210
m.kalenishin <m.kalenishin@ylab.io>
2022-07-22 21:33:11 +0300

    начальный коммит в ветке

--- local/modules/ylab.meetings/install/components/ylab/meeting.edit/lang/ru/.description.php
+++
@@ -1,3 +0,0 @@
-<?php
-$MESS['YLAB_MEETING_EDIT_EDIT_MEETING'] = 'Редактирование переговорной';
-$MESS['YLAB_MEETING_EDIT_COMPONENT_FROM'] = 'Компоненты от профессионалов';
--- local/modules/ylab.meetings/install/components/ylab/meetings.list/lang/ru/class.php
+++
@@ -1,3 +0,0 @@
-<?php
-$MESS['YLAB_MEETING_LIST_CLASS_EDIT'] = 'Редактировать';
-$MESS['YLAB_MEETING_LIST_CLASS_DELETE'] = 'Удалить';
