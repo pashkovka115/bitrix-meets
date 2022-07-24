81a57168dec32d4b14366bda0b025b2b95dd49e3
m.kalenishin <m.kalenishin@ylab.io>
2022-07-25 02:07:02 +0300

    Добавил компонент meeting.add и использовал его в шаблоне для add

---
+++ local/components/ylab/meeting.add/lang/ru/.parameters.php
@@ -0,0 +1,3 @@
+<?
+$MESS['YLAB_MEETING_ADD_OUTPUT_VARS'] = 'Входные переменные';
+$MESS['YLAB_MEETING_ADD_ID_MEETING'] = 'ID переговорной';
---
+++ local/components/ylab/meeting.add/lang/ru/class.php
@@ -0,0 +1,1 @@
+<?$MESS['YLAB_MEETING_ADD_ERROR_CHECK_COMPONENT'] = 'Не загружены модули необходимые для работы компонента';
