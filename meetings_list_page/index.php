e7413803136adc805e6a8de9b332265e19a69f72
m.kalenishin <m.kalenishin@ylab.io>
2022-07-27 12:50:21 +0300

    изменил meeting_list_page

--- meetings_list_page/index.php
+++ meetings_list_page/index.php
@@ -1,13 +1,25 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 $APPLICATION->SetTitle("");
-?><?$APPLICATION->IncludeComponent(
-	"ylab:meetings.list",
-	"grid",
-	Array(
-		"COLUMN_FIELDS" => array("ID","NAME","ACTIVITY","INTEGRATION.NAME",""),
-		"FILTER_FIELDS" => array("ID","NAME","ACTIVITY",""),
-		"LIST_ID" => "rooms_list",
-		"ORM_NAME" => "RoomTable"
-	)
-);?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+?><? $APPLICATION->IncludeComponent(
+  "ylab:meetings.list",
+  "grid",
+  array(
+    "COLUMN_FIELDS" => array(
+      0 => "ID",
+      1 => "NAME",
+      2 => "ACTIVITY",
+      3 => "INTEGRATION.NAME",
+      4 => "CALENDAR_TYPE_XML_ID",
+    ),
+    "FILTER_FIELDS" => array(
+      0 => "ID",
+      1 => "NAME",
+      2 => "ACTIVITY",
+    ),
+    "LIST_ID" => "rooms_list",
+    "ORM_NAME" => "RoomTable",
+    "COMPONENT_TEMPLATE" => "grid"
+  ),
+  false
+); ?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
