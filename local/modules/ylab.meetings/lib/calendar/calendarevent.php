2888d1c65075187bbd2d7babcae4b0df42a0bd70
Sergey <s.smirnov@ylab.io>
2022-07-27 15:41:42 +0300

    Исправил выборку Hiblock с ID на имя

--- local/modules/ylab.meetings/lib/calendar/calendarevent.php
+++ local/modules/ylab.meetings/lib/calendar/calendarevent.php
@@ -4,6 +4,7 @@ namespace Ylab\Meetings\Calendar;
 
 use Bitrix\Main\Loader;
 use CCalendarEvent;
+use Bitrix\Highloadblock as HL;
 
 /**
  * Class Event
@@ -18,12 +19,32 @@ class CalendarEvent
     {
         if (Loader::includeModule("calendar")) {
             $event = CCalendarEvent::GetById($id);
-            if($event["CAL_TYPE"] == "user") {
-                $event["DESCRIPTION"] = $event["NAME"];
-                CCalendarEvent::edit(array("arFields" => $event));
+            if ($event["CAL_TYPE"] == "user") {
+                self::addEvent([
+                    'UF_ID_EVENT_CALENDAR' => $event['ID'],
+                    'UF_ID_ROOM' => rand(100, 10000),
+                    'UF_CALENDAR_TYPE_XML_ID' => $event['CAL_TYPE'],
+                    'UF_URL_START' => 'http://' . bin2hex(random_bytes(10)) . '.site.com',
+                ]);
+
+                CCalendarEvent::edit(["arFields" => $event]);
             }
         }
+    }
 
+
+    public static function addEvent(array $data)
+    {
+        Loader::includeModule('highloadblock');
+
+        $hlblock = HL\HighloadBlockTable::getList([
+            'filter' => ['=NAME' => 'YlabMeetingsZoom']
+        ])->fetch();
+
+        $entity = HL\HighloadBlockTable::compileEntity($hlblock);
+        $class = $entity->getDataClass();
+
+        $class::add($data);
     }
 
 }
